f31114391bc5e1227697e34a8ecae5316d0bbadb
Artem <kuschenkoartem52@gmail.com>
2023-06-27 23:10:59 +0300

    Task

---
+++ Triadi/Triadi.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const char* str = "abcdef";
+    int len = strlen(str);
+    char* newStr = new char[len * 2 + 1];
+    char* ptr = newStr;
+
+    for (const char* p = str; p < str + len; p += 3) {
+        *ptr++ = *p;
+        *ptr++ = *(p + 1);
+        *ptr++ = *(p + 2);
+        *ptr++ = *p;
+        *ptr++ = *(p + 1);
+        *ptr++ = *(p + 2);
+    }
+    *ptr = '\0';
+
+    cout << newStr << endl;
+
+    delete[] newStr;
+    return 0;
+}
+
+
+
